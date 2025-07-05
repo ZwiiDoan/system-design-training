cce6e8acbbc12457ce085a1e31e9a17f2da1cd74
zwiid <zwii.doan@gmail.com>
2025-07-05 20:57:14 +1000

    Remove IAM access key outputs from Terraform configuration

--- aws/week-1/lab-bootstrap-iam/outputs.tf
+++ aws/week-1/lab-bootstrap-iam/outputs.tf
@@ -8,18 +8,6 @@ output "iam_user_arn" {
   description = "The ARN of the created IAM user."
 }
 
-output "iam_access_key_id" {
-  value       = aws_iam_access_key.terraform_user_key.id
-  description = "The access key ID for the IAM user."
-  sensitive   = true
-}
-
-output "iam_secret_access_key" {
-  value       = aws_iam_access_key.terraform_user_key.secret
-  description = "The secret access key for the IAM user."
-  sensitive   = true
-}
-
 output "secretsmanager_secret_arn" {
   value       = aws_secretsmanager_secret.terraform_user_creds.arn
   description = "The ARN of the Secrets Manager secret storing the credentials."
