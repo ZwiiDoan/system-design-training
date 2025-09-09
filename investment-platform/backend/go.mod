5743aea53f51a8fa7685878ddbd725a420fb5b12
zwiid <zwii.doan@gmail.com>
2025-09-09 20:40:37 +1000

    AI Solution Architect - M1W2D2

--- investment-platform/backend/go.mod
+++ investment-platform/backend/go.mod
@@ -1,3 +1,8 @@
 module backend
 
 go 1.25.0
+
+require (
+	github.com/go-resty/resty/v2 v2.16.5 // indirect
+	golang.org/x/net v0.33.0 // indirect
+)
--- investment-platform/backend/service/price.go
+++ investment-platform/backend/service/price.go
@@ -1,9 +1,9 @@
 package service
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
+
+	"github.com/go-resty/resty/v2"
 )
 
 type TickerResponse struct {
@@ -13,15 +13,14 @@ type TickerResponse struct {
 
 func GetPrice(symbol string) (TickerResponse, error) {
 	url := fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=%s", symbol)
-	resp, err := http.Get(url)
+	client := resty.New()
+	var ticker TickerResponse
+	resp, err := client.R().SetResult(&ticker).Get(url)
 	if err != nil {
 		return TickerResponse{}, err
 	}
-	defer resp.Body.Close()
-
-	var ticker TickerResponse
-	if err := json.NewDecoder(resp.Body).Decode(&ticker); err != nil {
-		return TickerResponse{}, err
+	if resp.IsError() {
+		return TickerResponse{}, fmt.Errorf("API error: %s", resp.Status())
 	}
 	return ticker, nil
 }
